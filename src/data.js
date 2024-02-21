cc21683a022725e81a67efac39b78c625239591c
yatinmehta <93468770+yatinmehta@users.noreply.github.com>
2024-02-21 19:02:11 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,12 @@
+export const API_KEY = 'AIzaSyC_iGYhBfKHJScC1iyPOUVpyfeHXwJtMgs';
+
+export const value_converter = (value) =>{
+    if(value>=1000000){
+        return Math.floor(value/1000000)+"M";
+    }
+    else if(value>=1000){
+        return Math.floor(value/1000)+"K";
+    }else{
+        return value;
+    }
+}
